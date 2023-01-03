6ff2433441636cd4a289db5fbc6d1f1d7f4febc6
iscodand <iscodand@outlook.com>
2023-01-03 16:25:47 -0300

    add: start user api

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -6,7 +6,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -17,4 +17,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs'
     ),
+    path('api/user', include('user.urls'))
 ]
