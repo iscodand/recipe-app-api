a35328320aefdda7053894e3204508cb455abf70
iscodand <iscodand@outlook.com>
2023-01-04 17:02:22 -0300

    fix: user's endpoints

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs'
     ),
-    path('api/user', include('user.urls'))
+    path('api/user/', include('user.urls'))
 ]
