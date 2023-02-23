21bee2983498df7e1630e3a57257ece8436cb047
iscodand <iscodand@outlook.com>
2023-02-23 09:58:39 -0300

    models and migrations: add ImageField to recipe

--- app/core/models/recipe.py
+++ app/core/models/recipe.py
@@ -1,8 +1,19 @@
 """
 Recipe Model.
 """
+import uuid
+import os
+
 from django.conf import settings
 from django.db import models
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class Recipe(models.Model):
@@ -18,6 +29,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
